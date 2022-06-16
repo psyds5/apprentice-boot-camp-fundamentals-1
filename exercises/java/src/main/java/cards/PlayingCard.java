81ae3276285b0187c759e776a4bb967a24c56310
Della Sam <della.sam@autotrader.co.uk>
2022-06-16 12:32:16 +0100

    add suit class and use in Card class

--- exercises/java/src/main/java/cards/PlayingCard.java
+++ exercises/java/src/main/java/cards/PlayingCard.java
@@ -10,15 +10,8 @@ public class PlayingCard {
     }
 
     public String getSuit(){
-        String suitName;
-        switch (suit){
-            case 0: suitName = "clubs"; break;
-            case 1: suitName = "diamonds"; break;
-            case 2: suitName = "hearts"; break;
-            case 3: suitName = "spades"; break;
-            default: throw new IllegalArgumentException("Something went wrong " + suit + "is not a valid suitName!");
-        }
-        return suitName;
+        Suit suitClass = new Suit(suit);
+        return suitClass.getSuite();
     }
 
     public String getFaceValue() {
---
+++ exercises/java/src/main/java/cards/Suit.java
@@ -0,0 +1,19 @@
+package cards;
+
+public class Suit {
+    String name;
+
+    public Suit(int suitNumber){
+        switch (suitNumber){
+            case 0: name = "clubs"; break;
+            case 1: name = "diamonds"; break;
+            case 2: name = "hearts"; break;
+            case 3: name = "spades"; break;
+            default: throw new IllegalArgumentException("Something went wrong " + suitNumber + "is not a valid suitName!");
+        }
+    }
+
+    public String getSuite(){
+        return name;
+    }
+}
