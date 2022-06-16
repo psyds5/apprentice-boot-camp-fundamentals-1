94f5fb5818436eb75f87fe0557c605dc54c5e512
Della Sam <della.sam@autotrader.co.uk>
2022-06-16 14:01:57 +0100

    add playingcardsdeck class

--- exercises/java/src/main/java/cards/Cards.java
+++ exercises/java/src/main/java/cards/Cards.java
@@ -10,27 +10,7 @@ public class Cards {
     }
 
     String[] getCards() {
-        String[] result = new String[52];
-        int[][] deck = new int[52][2];
-
-        for (int suit = 0; suit < 4; suit++) {
-            for (int faceValue = 0; faceValue < 13; faceValue++) {
-                deck[suit*13+faceValue] = new int[]{suit, faceValue};
-            }
-        }
-
-        int cardNumber = 0;
-        for (int[] card : deck) {
-            PlayingCard playingCard = new PlayingCard(card[0], card[1]);
-            String faceValueName;
-            faceValueName = playingCard.getFaceValue();
-
-            String suitName;
-            suitName = playingCard.getSuit();
-            result[cardNumber] = faceValueName + " of " + suitName;
-            cardNumber++;
-        }
-
-        return result;
+        PlayingCardDeck playingCardDeck = new PlayingCardDeck(52);
+        return playingCardDeck.createDeckOfCards();
     }
 }
---
+++ exercises/java/src/main/java/cards/PlayingCardDeck.java
@@ -0,0 +1,33 @@
+package cards;
+
+public class PlayingCardDeck {
+    PlayingCard[] cards;
+    String[] result;
+
+    public PlayingCardDeck(int numberOfCards){
+        cards = new PlayingCard[numberOfCards];
+        result = new String[numberOfCards];
+    }
+
+    public String[] createDeckOfCards(){
+
+        for (int suit = 0; suit < 4; suit++) {
+            for (int faceValue = 0; faceValue < 13; faceValue++) {
+                cards[suit*13+faceValue] = new PlayingCard(suit, faceValue);
+            }
+        }
+
+        int cardNumber = 0;
+        for (PlayingCard card : cards) {
+            String faceValueName;
+            faceValueName = card.getFaceValue();
+
+            String suitName;
+            suitName = card.getSuit();
+            result[cardNumber] = faceValueName + " of " + suitName;
+            cardNumber++;
+        }
+
+        return result;
+    }
+}
